ca6cedbaf16144742369e33ef5b2b8751484cd86
Diyorbek2008 <diyorbekabdumannonov4@gmail.com>
2022-02-25 18:42:01 +0500

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ export default function Navbar() {
                 <div className='w-[48px] h-[40px] cursor-pointer'>
                     <img src={logo} alt="logo" />
                 </div>
-                <h3 className='navbarLink'>Home</h3>
-                <h3 className='navbarLink'>My Work</h3>
+                <h3 className='navbarLink hidden lg:block'>Home</h3>
+                <h3 className='navbarLink hidden lg:block'>My Work</h3>
                 <form className='w-[887px] bg-gray-light rounded-[10px] hidden lg:flex' onSubmit={e => {
                     e.preventDefault()
                     const searchValue = e.target.input.value
@@ -30,52 +30,58 @@ export default function Navbar() {
                         <img src={searchIcon} alt="searchIcon" />
                     </button>
                 </form>
-                <div className='wMedium'>
+                <div className='wMedium hidden lg:block'>
                     <img src={cart} alt="cart" />
                 </div>
-                <div className='wMedium'>
+                <div className='wMedium hidden lg:block'>
                     <img src={notifications} alt="notifications" />
                 </div>
-                <div className='wMedium'>
+                <div className='wMedium hidden lg:block'>
                     <img src={user} alt="user" />
                 </div>
-                <button className='w-[40px] h-[35px] cursor-pointer block lg:hidden' onClick={() => setNavbarToggled(!NavbarToggled)}>
+                <button className='w-[40px] h-[35px] cursor-pointer block lg:hidden'>
                     <img src={menuIcon} alt="menuIcon" />
                 </button>
             </div>
-            <div className='block lg:hidden bg-blue-dark py-[11px] px-[40px] items-center max-w-desktop mx-auto'>
-            <div className='flex justify-between'>
-            <div className='w-[48px] h-[40px] cursor-pointer'>
-                    <img src={logo} alt="logo" />
-                </div>
-                <button className='w-[40px] h-[35px] cursor-pointer'>
-                    <img src={menuIcon} alt="menuIcon" />
-                </button>
-                </div>
-                <h3 className='navbarLink'>Home</h3>
-                <h3 className='navbarLink'>My Work</h3>
-                <form className='w-[887px] bg-gray-light rounded-[10px] hidden lg:flex' onSubmit={e => {
-                    e.preventDefault()
-                    const searchValue = e.target.input.value
-                    e.target.input.value = ''
-                    console.log(searchValue)
-                }}>
-                    <select className='m-[1px] cursor-pointer bg-gray-medium py-[11px] px-[13px] outline-none rounded-l-[10px] form-select font-normal text-[14px] text-gray-highter'>
-                        <option value="Node type" >Node type</option>
-                    </select>
-                    <input type="text" name='input' placeholder='Search something...' className='bg-transparent outline-none w-full p-[14px] text-[14px] font-normal text-blue-medium' />
-                    <button className='bg-yellow-medium p-[15px] rounded-r-[10px]'>
-                        <img src={searchIcon} alt="searchIcon" />
+            <div className={'lg:hidden bg-blue-dark py-[11px] px-[40px] items-center max-w-desktop mx-auto'}>
+                <div className='flex justify-between'>
+                    <div className='w-[48px] h-[40px] cursor-pointer'>
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <button className='w-[40px] h-[35px] cursor-pointer block' onClick={() => setNavbarToggled(!NavbarToggled)}>
+                        <img src={menuIcon} alt="menuIcon" />
                     </button>
-                </form>
-                <div className='wMedium'>
-                    <img src={cart} alt="cart" />
                 </div>
-                <div className='wMedium'>
-                    <img src={notifications} alt="notifications" />
-                </div>
-                <div className='wMedium'>
-                    <img src={user} alt="user" />
+                <div className={'text-center ' + (NavbarToggled ? 'block' : 'hidden')}>
+                    <div className='flex justify-evenly flex-col sm:flex-row my-[10px]'>
+                        <h3 className='navbarLink'>Home</h3>
+                        <h3 className='navbarLink'>My Work</h3>
+                        <div className='flex justify-around w-full sm:w-1/3 mx-auto sm:mx-0'>
+                            <div className='wMedium'>
+                                <img src={cart} alt="cart" />
+                            </div>
+                            <div className='wMedium'>
+                                <img src={notifications} alt="notifications" />
+                            </div>
+                            <div className='wMedium'>
+                                <img src={user} alt="user" />
+                            </div>
+                        </div>
+                    </div>
+                    <form className='w-full bg-gray-light rounded-[10px] flex' onSubmit={e => {
+                        e.preventDefault()
+                        const searchValue = e.target.input.value
+                        e.target.input.value = ''
+                        console.log(searchValue)
+                    }}>
+                        <select className='m-[1px] cursor-pointer bg-gray-medium py-[11px] px-[5px] sm:px-[13px] outline-none rounded-l-[10px] form-select font-normal text-[14px] text-gray-highter'>
+                            <option value="Node type" >Node type</option>
+                        </select>
+                        <input type="text" name='input' placeholder='Search something...' className='bg-transparent outline-none w-full p-[14px] text-[14px] font-normal text-blue-medium' />
+                        <button className='bg-yellow-medium p-[15px] rounded-r-[10px]'>
+                            <img src={searchIcon} alt="searchIcon" />
+                        </button>
+                    </form>
                 </div>
             </div>
         </>
